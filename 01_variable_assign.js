db24d381c7f0404ed79f9f6bdf170cf1695f4bb7
ersonarneha <ersonarneha93@gmail.com>
2022-12-09 00:45:41 +0530

    Javascript Assignment 1
    Assignment on-Declaration,Initialization and update,console

---
+++ 01_variable_assign.js
@@ -0,0 +1,18 @@
+var myfullName;
+myfullName ="Neha Sonar";
+console.log(myfullName);
+
+myfullName ="Neha Suresh Sonar";
+console.log(myfullName);
+
+var myLove="I love only JS";
+console.log(myLove);
+
+var firstName="Neha";
+var lastName="Sonar";
+var age="28";
+var address=" Gandharv Nagari,Moshi,Pune";
+var pin_code="412105";
+var gender="Female";
+var is_married="Yes";
+console.log(firstName,lastName,age,address,pin_code,gender,is_married);

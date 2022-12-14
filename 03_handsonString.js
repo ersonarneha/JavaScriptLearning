73d5853c27af3aea1494bb94f666a3524743ca88
ersonarneha <ersonarneha93@gmail.com>
2022-12-15 00:22:42 +0530

    String template assignments1,2
    String template assignments1,2

---
+++ 03_handsonString.js
@@ -0,0 +1,14 @@
+var stringHandsOn = function stringTemplateAssign() {
+  console.log(`-------By using string template-------`);
+  console.log(`My dream company is "TCS"`);
+  console.log(`----by using variable substitution------`);
+  var hobby1 = "Badminton";
+  var hobby2 = "Reading";
+  var hobby3 = "Travelling";
+  console.log(`My hobbies are: ${hobby1} ${hobby2} and ${hobby3}`);
+
+  console.log(`---by using concatenation-----`);
+  console.log(`My hobbies are:` + hobby1 + hobby2 + hobby3);
+};
+
+stringHandsOn();
